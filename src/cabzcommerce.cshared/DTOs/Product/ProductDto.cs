1919cbcbaa529271564a80222f255463f71ab092
ecabigting <ericcabigting@outlook.com>
2022-03-05 10:12:46 +0400

    updated product dto update product added property product type as an object

--- src/cabzcommerce.cshared/DTOs/Product/ProductDto.cs
+++ src/cabzcommerce.cshared/DTOs/Product/ProductDto.cs
@@ -1,9 +1,18 @@
 using System.ComponentModel.DataAnnotations;
+using cabzcommerce.cshared.Models;
 
 namespace cabzcommerce.cshared.DTOs.Product
 {
     public class ProductDto
     {
-
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int MyProperty { get; set; }
+        public string ProductImgeDesktopUrl { get; set; }
+        public string ProductImgMobileUrl { get; set; }
+        public List<ProductType> Type { get; set; }    
+    
+    
     }
 }
